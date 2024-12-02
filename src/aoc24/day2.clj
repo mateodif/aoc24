3efafd89fb4600ef49ee02825e4df432cc90257d
Mateo Difranco <mateodifr@gmail.com>
2024-12-02 18:56:15 -0300

    Day 2

---
+++ src/aoc24/day2.clj
@@ -0,0 +1,62 @@
+(ns aoc24.day2
+  (:require [clojure.string :as str]))
+
+(def input
+  (->> "resources/day2"
+       slurp
+       str/split-lines))
+
+(defn format [input]
+  (map #(map parse-long (str/split % #" ")) input))
+
+(defn in-range-differs [levels]
+  (every? true?
+          (map (fn [[x y]] (<= 1 (abs (- x y)) 3))
+               (partition 2 1 levels))))
+
+(defn safe? [levels]
+  (and (or (apply < levels) (apply > levels))
+       (in-range-differs levels)))
+
+(defn safe-reports [input]
+  (->> input
+       format
+       (filter safe?)
+       count))
+
+
+;;;; Part 2
+
+(defn bad-level-by [f parts]
+  (->> parts
+       (filter (fn [[a b]] (not (f a b))))
+       first
+       second))
+
+(defn bad-levels [levels]
+  (let [parts (partition 2 1 levels)]
+    ((juxt (partial bad-level-by >)
+           (partial bad-level-by <)
+           (partial bad-level-by (fn [x y] (<= 1 (abs (- x y)) 3))))
+     parts)))
+
+(defn tolerable-safe? [levels]
+  (let [bad-levels (bad-levels levels)]
+    (->> bad-levels
+         (map (fn [bad-level] (safe? (remove #(= % bad-level) levels))))
+         (some true?))))
+
+(defn tolerable-reports [input]
+  (->> input
+       format
+       (filter #(or (safe? %) (tolerable-safe? %)))
+       count))
+
+(comment
+  ;; Part 1
+  (safe-reports input)
+
+  ;; Part 2
+  (tolerable-reports input)
+
+  )
